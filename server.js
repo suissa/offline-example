bb761b050be7981f1bbf3e97a7780b2f60472fa0
Kaique da Silva <kaique.developer@gmail.com>
2012-10-18 00:56:05 -0300

    backend tools

--- server.js
+++
@@ -1,9 +0,0 @@
-var httpServer = require('http').createServer(function(req, response){ /* Serve your static files */ })
-httpServer.listen(8080);
-
-var nowjs = require("now");
-var everyone = nowjs.initialize(httpServer);
-
-everyone.now.logStuff = function(msg){
-    console.log(msg);
-}
